159bbc31ce388a928a2d615f529e5d11ddee669c
markandmark1111@gmail.com <markandmark1111@gmail.com>
2021-06-04 01:01:39 +0200

    Add static class InverseTrigonometricFunctions..... With some of its unit tests.

---
+++ ComplexNumbersComprehensiveProgram/InverseTrigonometricFunctions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComplexNumbersComprehensiveProgram
+{
+    public static class InverseTrigonometricFunctions
+    {
+        public static double ArcTanRadian(double xScale, double yScale)
+        {
+            double angleRadian;
+
+            double xScaleABS = Math.Abs(xScale);
+            double yScaleABS = Math.Abs(yScale);
+            double angleRadianABS = Math.Atan2(yScaleABS, xScaleABS);
+
+            switch (QuadrantPosition.GetQuadrantPosition(xScale, yScale))
+            {
+                case "First quadrant":
+                    angleRadian = angleRadianABS;
+                    break;
+                case "Second quadrant":
+                    angleRadian = angleRadianABS + ((Math.PI) / 2);
+                    break;
+                case "Third quadrant":
+                    angleRadian = angleRadianABS + Math.PI;
+                    break;
+                case "Fourth quadrant":
+                    angleRadian = angleRadianABS + (Math.PI) * (1.5);
+                    break;
+                case "Positive part of X-Axis":
+                    angleRadian = 0;
+                    break;
+                case "Negative part of X-Axis":
+                    angleRadian = Math.PI;
+                    break;
+                case "Positive part of Y-Axis":
+                    angleRadian = (Math.PI) / 2;
+                    break;
+                case "Negative part of Y-Axis":
+                    angleRadian = (Math.PI) * 3 / 2;
+                    break;
+                default: // "Origin"
+                    angleRadian = 0;
+                    break;
+            }
+            return angleRadian;
+        }
+
+        public static double ArcTanDegree(double xScale, double yScale)
+        {
+            double angleRadian = InverseTrigonometricFunctions.ArcTanRadian(xScale, yScale);
+            double angleDegree = AngleOperations.Rad2Deg(angleRadian);
+            return angleDegree;
+        }
+    }
+}
---
+++ ComplexNumbersComprehensiveProgramTests/InverseTrigonometricFunctionsTests.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ComplexNumbersComprehensiveProgram;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComplexNumbersComprehensiveProgram.Tests
+{
+    [TestClass()]
+    public class InverseTrigonometricFunctionsTests
+    {
+        [TestMethod()]
+        public void ArcTanRadianTest1()
+        {
+            // Arrange
+            double xPositive = 3;
+            double yPositive = 4;
+            double expected = Math.Atan2(4,3);
+
+            //Act
+            double actual = InverseTrigonometricFunctions.ArcTanRadian(xPositive, yPositive);
+
+            //Assert
+            Assert.AreEqual(expected, actual, "Error arctan measure");
+        }
+
+        // ==========================================================================
+
+        [TestMethod()]
+        public void ArcTanDegreeTest1()
+        {
+            // Arrange
+            double xPositive = 1;
+            double yPositive = 1;
+            double expected = 45;
+
+            //Act
+            double actual = InverseTrigonometricFunctions.ArcTanDegree(xPositive, yPositive);
+
+            //Assert
+            Assert.AreEqual(expected, actual, "Error arctan measure");
+        }
+        [TestMethod()]
+        public void ArcTanDegreeTest2()
+        {
+            // Arrange
+            double xPositive = 0;
+            double yPositive = 1;
+            double expected = 90;
+
+            //Act
+            double actual = InverseTrigonometricFunctions.ArcTanDegree(xPositive, yPositive);
+
+            //Assert
+            Assert.AreEqual(expected, actual, "Error arctan measure");
+        }
+        [TestMethod()]
+        public void ArcTanDegreeTest3()
+        {
+            // Arrange
+            double xPositive = 1;
+            double yPositive = 0;
+            double expected = 0;
+
+            //Act
+            double actual = InverseTrigonometricFunctions.ArcTanDegree(xPositive, yPositive);
+
+            //Assert
+            Assert.AreEqual(expected, actual, "Error arctan measure");
+        }
+        [TestMethod()]
+        public void ArcTanDegreeTest4()
+        {
+            // Arrange
+            double xPositive = 0;
+            double yPositive = 0;
+            double expected = 0;
+
+            //Act
+            double actual = InverseTrigonometricFunctions.ArcTanDegree(xPositive, yPositive);
+
+            //Assert
+            Assert.AreEqual(expected, actual, "Error arctan measure");
+        }
+        [TestMethod()]
+        public void ArcTanDegreeTest5()
+        {
+            // Arrange
+            double xPositive = -1;
+            double yPositive = 0;
+            double expected = 180;
+
+            //Act
+            double actual = InverseTrigonometricFunctions.ArcTanDegree(xPositive, yPositive);
+
+            //Assert
+            Assert.AreEqual(expected, actual, "Error arctan measure");
+        }
+        [TestMethod()]
+        public void ArcTanDegreeTest6()
+        {
+            // Arrange
+            double xPositive = 0;
+            double yPositive = -4;
+            double expected = 270;
+
+            //Act
+            double actual = InverseTrigonometricFunctions.ArcTanDegree(xPositive, yPositive);
+
+            //Assert
+            Assert.AreEqual(expected, actual, "Error arctan measure");
+        }
+
+        [TestMethod()]
+        public void ArcTanDegreeTest7()
+        {
+            // Arrange
+            double xPositive = -1;
+            double yPositive = -1;
+            double expected = 270-45;
+
+            //Act
+            double actual = InverseTrigonometricFunctions.ArcTanDegree(xPositive, yPositive);
+
+            //Assert
+            Assert.AreEqual(expected, actual, "Error arctan measure");
+        }
+
+        [TestMethod()]
+        public void ArcTanDegreeTest8()
+        {
+            // Arrange
+            double xPositive = 1;
+            double yPositive = -1;
+            double expected = 270 + 45;
+
+            //Act
+            double actual = InverseTrigonometricFunctions.ArcTanDegree(xPositive, yPositive);
+
+            //Assert
+            Assert.AreEqual(expected, actual, "Error arctan measure");
+        }
+
+        [TestMethod()]
+        public void ArcTanDegreeTest9()
+        {
+            // Arrange
+            double xPositive = -1;
+            double yPositive = -1;
+            double expected = 270 - 45;
+
+            //Act
+            double actual = InverseTrigonometricFunctions.ArcTanDegree(xPositive, yPositive);
+
+            //Assert
+            Assert.AreEqual(expected, actual, "Error arctan measure");
+        }
+    }
+}
