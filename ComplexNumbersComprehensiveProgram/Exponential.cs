f288d7fc8e1a84cedf49437ee1ba5f07363be53d
markandmark1111@gmail.com <markandmark1111@gmail.com>
2021-06-22 21:30:04 +0200

    Add public static class Exponential.

---
+++ ComplexNumbersComprehensiveProgram/Exponential.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComplexNumbersComprehensiveProgram
+{
+    public static class Exponential
+    {
+        public static PolarRadianNumber ExponentialFunction(PolarRadianNumber number)
+        {
+            var res = Power.RealNumberComplexPower(Math.E, number);
+            return res;
+        }
+
+        public static PolarDegreeNumber ExponentialFunction(PolarDegreeNumber number)
+        {
+            var res = Power.RealNumberComplexPower(Math.E, number);
+            return res;
+        }
+
+        public static XYFormNumber ExponentialFunction(XYFormNumber number)
+        {
+            var res = Power.RealNumberComplexPower(Math.E, number);
+            return res;
+        }
+    }
+}
